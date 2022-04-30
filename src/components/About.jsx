85524afbac337abc64eb2d82c6c78ea43a3c0c2a
Zavim <Zark471@gmail.com>
2022-04-29 20:37:38 -0400

    -f add about page

---
+++ src/components/About.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import placeholder from "../images/image-placeholder.png";
+import placeholderVert from "../images/image-placeholder-vert.png";
+
+export default function About() {
+	//make this a drop down with focus
+	return (
+		<>
+			<nav className="mission-container">
+				<nav className="mission-container-text">
+					<h2>mission statement</h2>
+					<p>
+						Fever Dream is dedicated to supporting the careers of artists, in
+						turn, cultivating and advancing fine art in Cleveland.
+					</p>
+				</nav>
+				<img src={placeholder} alt="placeholder"></img>
+			</nav>
+			<nav className="vision-container">
+				<img src={placeholder} alt="placeholder" />
+				<nav className="vision-container-text">
+					<h2>vision</h2>
+					<p>
+						Fever Dream’s vision is to fill a niche within the artistic
+						community of Cleveland as an experimental space for artists to
+						thrive and grow, celebrating the following values: creative
+						development, collaboration, learning, and community. At the heart of
+						this vision lies the Fever Dream Residency program which offers
+						artists time, space, and funds to pursue the work they’re passionate
+						about. The program is particularly geared towards young, emerging 2D
+						artists within the scope of painting and drawing. Resident artists’
+						practices will thrive within the Fever Dream. And their admittance
+						into the program in addition to their contributions to Fever Dreams
+						ongoing projects will key them into a network of artists for a
+						lifetime.
+					</p>
+					<h3>creative development</h3>
+					<p>
+						Providing the tools for career and research mobility to emerging
+						artists as well as celebrating their artistic practices.
+					</p>
+					<h3>collaboration</h3>
+					<p>
+						Passionately listening to, working with, and supporting others to
+						achieve a common goal.
+					</p>
+					<h3>learning</h3>
+					<p>
+						Remaining open-minded for the sake of positive growth to better
+						serve artists. Experimenting with new ideas and models.
+					</p>
+					<h3>community</h3>
+					<p>
+						Vitalizing the creative community of Cleveland by bringing members
+						closer together and supporting them.{" "}
+					</p>
+				</nav>
+			</nav>
+			<nav className="board-container">
+				<h2>board</h2>
+				<nav className="board-bio">
+					<nav className="board-bio-image-container">
+						<img src={placeholderVert} alt="placeholder" />
+						<h3>jesse grant</h3>
+						<h4>Founder, Head of the board</h4>
+					</nav>
+					<nav className="haiku-container">
+						<p className="haiku">
+							the universe in a spirited silhouette with a beating heart.
+						</p>
+						<p className="haiku-more">More</p>
+					</nav>
+				</nav>
+			</nav>
+		</>
+	);
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Navbar() {
+	return (
+		<>
+			<nav className="navbar">
+				<a href="/">
+					<h3>feverdream</h3>
+				</a>
+				<a href="/about">
+					<h3>about</h3>
+				</a>
+				<a href="/residency">
+					<h3>residency</h3>
+				</a>
+				<a href="/dreamJournal">
+					<h3>dreamjournal</h3>
+				</a>
+				<a href="/support">
+					<h3>support</h3>
+				</a>
+			</nav>
+		</>
+	);
+}
