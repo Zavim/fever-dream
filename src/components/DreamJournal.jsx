984fcf9499fd3014da80a68bde26802ed1924b04
Zavim <Zark471@gmail.com>
2022-06-13 16:03:15 -0400

    -c fix img placeholder paths

--- src/components/DreamJournal.jsx
+++ src/components/DreamJournal.jsx
@@ -1,79 +1,78 @@
 import React from "react";
-import placeholder from "../images/image-placeholder.png";
-import placeholderVert from "../images/image-placeholder-vert.png";
+import placeholder from "/images/image-placeholder.png";
 
 export default function DreamJournal() {
-	return (
-		<>
-			<h2 className="journal-title">dream journal</h2>
-			<nav className="journal-container">
-				<nav className="journal-entry">
-					<img src={placeholder} alt="entry-placeholder" />
-					<nav className="entry-text">
-						<p>
-							Our Thoughts on [Artists Name]’s Newest Exhibition at [Gallery
-							Name]
-						</p>
-						<p className="entry-readmore">Read More</p>
-					</nav>
-				</nav>
-				<nav className="journal-entry">
-					<img src={placeholder} alt="entry-placeholder" />
-					<nav className="entry-text">
-						<p>
-							Our Thoughts on [Artists Name]’s Newest Exhibition at [Gallery
-							Name]
-						</p>
-						<p className="entry-readmore">Read More</p>
-					</nav>
-				</nav>
-			</nav>
-			<p className="archive-button">Archive</p>
-			<nav className="news-container">
-				<h2>alumni news network</h2>
-				<p>
-					The feverdream “Dream Journal” is a platform made to showcase most
-					recent work
-					<br />
-					and careers of past residents. feverdream is devoted to is continual
-					support of those
-					<br />
-					who have been selected for the residency program.
-				</p>
-				<nav className="news-img-container">
-					<img
-						src={placeholder}
-						alt="alumni placeholder"
-						className="news-img"
-					/>
-					<img
-						src={placeholder}
-						alt="alumni placeholder"
-						className="news-img"
-					/>
-					<img
-						src={placeholder}
-						alt="alumni placeholder"
-						className="news-img"
-					/>
-					<img
-						src={placeholder}
-						alt="alumni placeholder"
-						className="news-img"
-					/>
-					<img
-						src={placeholder}
-						alt="alumni placeholder"
-						className="news-img"
-					/>
-					<img
-						src={placeholder}
-						alt="alumni placeholder"
-						className="news-img"
-					/>
-				</nav>
-			</nav>
-			<p className="more-button">More</p>
-		</>
-	);
+  return (
+    <>
+      <h2 className="journal-title">dream journal</h2>
+      <nav className="journal-container">
+        <nav className="journal-entry">
+          <img src={placeholder} alt="entry-placeholder" />
+          <nav className="entry-text">
+            <p>
+              Our Thoughts on [Artists Name]’s Newest Exhibition at [Gallery
+              Name]
+            </p>
+            <p className="entry-readmore">Read More</p>
+          </nav>
+        </nav>
+        <nav className="journal-entry">
+          <img src={placeholder} alt="entry-placeholder" />
+          <nav className="entry-text">
+            <p>
+              Our Thoughts on [Artists Name]’s Newest Exhibition at [Gallery
+              Name]
+            </p>
+            <p className="entry-readmore">Read More</p>
+          </nav>
+        </nav>
+      </nav>
+      <p className="archive-button">Archive</p>
+      <nav className="news-container">
+        <h2>alumni news network</h2>
+        <p>
+          The feverdream “Dream Journal” is a platform made to showcase most
+          recent work
+          <br />
+          and careers of past residents. feverdream is devoted to is continual
+          support of those
+          <br />
+          who have been selected for the residency program.
+        </p>
+        <nav className="news-img-container">
+          <img
+            src={placeholder}
+            alt="alumni placeholder"
+            className="news-img"
+          />
+          <img
+            src={placeholder}
+            alt="alumni placeholder"
+            className="news-img"
+          />
+          <img
+            src={placeholder}
+            alt="alumni placeholder"
+            className="news-img"
+          />
+          <img
+            src={placeholder}
+            alt="alumni placeholder"
+            className="news-img"
+          />
+          <img
+            src={placeholder}
+            alt="alumni placeholder"
+            className="news-img"
+          />
+          <img
+            src={placeholder}
+            alt="alumni placeholder"
+            className="news-img"
+          />
+        </nav>
+      </nav>
+      <p className="more-button">More</p>
+    </>
+  );
 }
