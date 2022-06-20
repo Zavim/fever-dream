6028aca78b197a7928b7a342d3cff00cf46a3215
Zavim <Zark471@gmail.com>
2022-06-20 18:52:46 -0400

    -f add faq dropdowns

---
+++ src/components/Collapsible.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import triButtonUp from "/images/tri-button-up.png";
+import triButtonDown from "/images/tri-button-down.png";
+
+export default function Collapsible(props) {
+  const { question, answer } = props;
+  const [open, setOpen] = useState(false);
+
+  const toggle = (open) => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="question" onClick={() => toggle(open)}>
+      <nav className="question-text">
+        <p>{question}</p>
+        <img
+          className="dropdown-button"
+          src={open ? triButtonUp : triButtonDown}
+          alt="triangle pointing down"
+        ></img>
+      </nav>
+      <nav className={open ? "answer open" : "answer"}>
+        <p>{answer}</p>
+      </nav>
+    </div>
+  );
+}
--- src/components/Residency.jsx
+++ src/components/Residency.jsx
@@ -5,6 +5,8 @@ import residents from "/images/residents.jpg";
 import about from "/images/about.jpg";
 import facilities from "/images/facilities.jpg";
 
+import Collapsible from "./Collapsible";
+
 export default function Residency() {
   return (
     <>
@@ -59,7 +61,6 @@ export default function Residency() {
       </nav>
       <nav className="residents-container">
         <img src={residents} alt="placeholder" />
-
         <nav className="residents-container-text">
           <h2>residents</h2>
           <p>
@@ -78,35 +79,78 @@ export default function Residency() {
       <nav className="faq-container">
         <nav className="faq-container-text">
           <h2>frequently asked questions</h2>
-          <p>
-            Why is the program called Fever Dream? <button></button>
-          </p>
-
-          <p>Who is eligible to apply?</p>
-          <p>
-            What is the vaccination policy in regards to incoming residents?
-          </p>
-          <p>I'm still in school. Can I apply?</p>
-          <p>How do I apply?</p>
-          <p>How often can I apply?</p>
-          <p>When are the deadlines and what are the residency seasons?</p>
-          <p>How do I give myself the best chance to be selected?</p>
-          <p>What is the cost of the residency?</p>
-          <p>How long is the residency?</p>
-          <p> How much is the stipend provided for the artist resident?</p>
-          <p> Are supplies provided?</p>
-          <p> Can I have guests in the studio?</p>
-          <p> Does Fever Dream provide meals for the artist resident?</p>
-          <p> How long after the deadline will my residency begin?</p>
-          <p> Who reviews my application?</p>
-          <p>
-            I applied and was not offered the residency. How do I make my
-            application stronger?
-          </p>
-          <p>
-            I applied and was not offered the residency. Can I reapply next time
-            applications open?
-          </p>
+          <Collapsible
+            question="Why is the program called Fever Dream?"
+            answer="😏"
+          />
+          <Collapsible
+            question="Who is eligible to apply?"
+            answer="The scope of Fever Dream includes any young, local, emerging artists with high potential that showcase excellence in the mediums of painting and drawing. Local is defined as Ohio-based within a 2 hour radius. Artist collectives will not be eligible. Fever Dream is an open call for artists to apply where they will be adjudicated on the quality and promise of their proposal."
+          />
+          <Collapsible
+            question="What is the vaccination policy in regards to incoming residents?"
+            answer="To ensure the safety of the local community and the safety of Fever Dream staff, the program requires the resident to be vaccinated."
+          />
+          <Collapsible
+            question="I'm still in school. Can I apply?"
+            answer="Yes, but Fever Dream requires that the artist applying be graduated from undergrad by the start of their residency term."
+          />
+          <Collapsible
+            question="How do I apply?"
+            answer="The Fever Dream application can be found [here]."
+          />
+          <Collapsible
+            question="How often can I apply?"
+            answer="There is no limit to how often one can apply but if you get selected you are no longer allowed to apply."
+          />
+          <Collapsible
+            question="When are the deadlines and what are the residency seasons?"
+            answer="Applications will be open for 2 months. The Phase One 2 month sessions are on a dynamic calendar where the break between each session can be at least a month or more. Be sure to join our email list to be notified when applications for the next session go live."
+          />
+          <Collapsible
+            question="How do I give myself the best chance to be selected?"
+            answer="The best advice to follow is that all applicants should make sure their applications are complete, their proposal is clear, and that the submitted work is strong and documented in a professional manner."
+          />
+          <Collapsible
+            question="What is the cost of the residency?"
+            answer="There is no cost associated with your participation as a resident. There is a nonrefundable application fee of $10 that is waivable if you are in need of financial assistance."
+          />
+          <Collapsible
+            question="How long is the residency?"
+            answer="The residency is 2 months long."
+          />
+          <Collapsible
+            question="How much is the stipend provided for the artist resident?"
+            answer="The stipend provided by Fever Dream is $3000."
+          />
+          <Collapsible
+            question="Are supplies provided?"
+            answer="Some supplies are provided in the studio space. We ask that the resident brings as much material as they can. If more supplies are needed Fever Dream will reimburse residents' costs up to $800."
+          />
+          <Collapsible
+            question="Can I have guests in the studio?"
+            answer="The only guests allowed into the studio are guests who can provide formal artist's studio visits."
+          />
+          <Collapsible
+            question="Does Fever Dream provide meals for the artist resident?"
+            answer="No, Fever Dream does not provide meals. There are plenty of dining options in the vicinity. A map of some of those establishments can be found [here.]"
+          />
+          <Collapsible
+            question="How long after the deadline will my residency begin?"
+            answer="There is roughly 1 month between when you are notified as being the final selected resident and when your residency starts. But because of our dynamic Phase One calendar there may be more time. You will be notified when your session will start if you are the selected resident."
+          />
+          <Collapsible
+            question="Who reviews my application?"
+            answer="The applications are reviewed by the board. The board is made up of Jesse Grant, Christine Grant, Maxmillian Peralta, Erin Guido, and John Paul Costello."
+          />
+          <Collapsible
+            question="I applied and was not offered the residency. How do I make my application stronger?"
+            answer="We are not able to provide specific individual feedback to applicants. To reiterate what was previously mentioned, make sure your application is complete , your proposal is clear, and the submitted work is strong and documented in a professional manner."
+          />
+          <Collapsible
+            question="I applied and was not offered the residency. Can I reapply next time applications open?"
+            answer="Yes, you can apply for the next season."
+          />
         </nav>
       </nav>
       <button className="apply-button">apply</button>
