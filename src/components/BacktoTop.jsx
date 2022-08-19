09d8e0e3469eea8e165f0f27658eff406b827c81
Zavim <Zark471@gmail.com>
2022-08-19 11:58:39 -0400

    -f add favicon and back to top

---
+++ src/components/BacktoTop.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function BacktoTop() {
+  //   let topButton;
+
+  //   window.onscroll = () => {
+  //     topButton = document.getElementById("back-to-top-button");
+  //     scrollFunction(topButton);
+  //   };
+
+  //   const scrollFunction = (topButton) => {
+  //     if (
+  //       document.body.scrollTop > 20 ||
+  //       document.documentElement.scrollTop > 20
+  //     ) {
+  //       topButton.style.display = "block";
+  //     } else {
+  //       topButton.style.display = "none";
+  //     }
+  //   };
+
+  const backToTop = () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  };
+
+  return (
+    <>
+      <button
+        onClick={backToTop}
+        className="back-to-top-button"
+        id="back-to-top-button"
+      >
+        TOP
+      </button>
+    </>
+  );
+}
