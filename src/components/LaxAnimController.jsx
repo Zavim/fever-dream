bbdbe92c9f6ac5178653e2e2fab9c6e25064e2ab
Zavim <Zark471@gmail.com>
2023-01-06 17:27:19 -0500

    -f rm img opacity on scroll

--- src/components/LaxAnimController.jsx
+++ src/components/LaxAnimController.jsx
@@ -41,75 +41,18 @@ export default function LaxAnimController(props) {
           ["elInY", "elOutY"],
           ["screenHeight/10", "-screenHeight/10"],
           {
-            inertia: -1,
+            inertia: -0.5,
           },
         ],
-        opacity: [
-          ["elInY", "elCenterY", "elOutY"],
-          [0, 1, 0.5],
-          {
-            easing: "easeInOutCubic",
-          },
-        ],
+        // opacity: [
+        //   ["elInY", "elCenterY", "elOutY"],
+        //   [0, 1, 0.5],
+        //   {
+        //     easing: "easeInOutCubic",
+        //   },
+        // ],
       },
     });
-
-    // lax.addElements(".mission-container", {
-    //   scrollY: {
-    //     opacity: [
-    //       ["elInY/5", "elOutY"],
-    //       [1, 0],
-    //     ],
-    //   },
-    // });
-    // lax.addElements(".vision-container", {
-    //   scrollY: {
-    //     opacity: [
-    //       ["elHeight", "elHeight*2.4"],
-    //       [1, 0],
-    //     ],
-    //   },
-    // });
-    // lax.addElements("#jesse-board-bio", {
-    //   scrollY: {
-    //     opacity: [
-    //       [1900, 2100],
-    //       [1, 0],
-    //     ],
-    //   },
-    // });
-    // lax.addElements("#christine-board-bio", {
-    //   scrollY: {
-    //     opacity: [
-    //       [2300, 2500],
-    //       [1, 0],
-    //     ],
-    //   },
-    // });
-    // lax.addElements("#erin-board-bio", {
-    //   scrollY: {
-    //     opacity: [
-    //       [2900, 3100],
-    //       [1, 0],
-    //     ],
-    //   },
-    // });
-    // lax.addElements("#johnPaul-board-bio", {
-    //   scrollY: {
-    //     opacity: [
-    //       [3400, 3600],
-    //       [1, 0],
-    //     ],
-    //   },
-    // });
-    // lax.addElements("#maxmillian-board-bio", {
-    //   scrollY: {
-    //     opacity: [
-    //       [4000, 4200],
-    //       [1, 0],
-    //     ],
-    //   },
-    // });
   }, []);
 
   return <>{elements}</>;
